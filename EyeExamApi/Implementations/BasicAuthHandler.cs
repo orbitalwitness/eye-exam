f3ff4b8e6a3f3988bd4127ca2791c14ff359fc14
Antony McKane <antony@orbitalwitness.com>
2022-04-13 16:58:27 +0100

    Added Auth

---
+++ EyeExamApi/Implementations/BasicAuthHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace EyeExamApi.Implementations
+{
+    public class BasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        /// <summary>
+        /// The index of the basic auth header token after "basic ".
+        /// </summary>
+        private const int TOKEN_START_INDEX = 6;
+
+        public BasicAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+
+        /// <summary>
+        /// Dummy custom auth handler, because mimimal APIs regressed in this a bit.
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return await Task.Run(() =>
+            {
+                var header = Request.Headers["Authorization"].ToString();
+                if (header?.Length > 0 && header.Contains("basic", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var authToken = header.Substring(TOKEN_START_INDEX).Trim();
+                    var creds = Encoding.UTF8.GetString(Convert.FromBase64String(authToken)).Split(':');
+                    if (creds.Length == 2 && creds.First() == "testy" && creds.Last() == "mcTestFace")
+                    {
+                        var claimsPrincipal = new ClaimsPrincipal(
+                            new ClaimsIdentity(new List<Claim>()
+                            {
+                                new Claim(ClaimTypes.Role, "User")
+                            }, "Basic")
+                        );
+
+                        return AuthenticateResult.Success(
+                            new AuthenticationTicket(claimsPrincipal, Scheme.Name)
+                        );
+                    }
+                }
+
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            });
+        }
+    }
+}
--- EyeExamApi/Program.cs
+++ EyeExamApi/Program.cs
@@ -1,10 +1,42 @@
+using EyeExamApi.Implementations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // DI
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication("basic")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthHandler>("basic", null);
+builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "BasicAuth", Version = "v1" });
+    o.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = ParameterLocation.Header,
+        Description = "Basic Authorization header."
+    });
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "basic"
+                    }
+                },
+                new string[] {}
+        }
+    });
+});
 builder.Services.AddScoped<IRawScheduleDataService, RawScheduleDataService>();
 builder.Services.AddScoped<IParsedScheduleDataService, ParsedScheduleDataService>();
 
@@ -18,12 +50,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/schedules", ([FromServices] IRawScheduleDataService rawScheduleDataService) => {
+app.MapGet("/schedules", [Authorize] ([FromServices] IRawScheduleDataService rawScheduleDataService) => {
     return rawScheduleDataService.GetRawScheduleNoticeOfLeases();
 });
 
-app.MapGet("/results", ([FromServices] IParsedScheduleDataService parsedScheduleDataService) => { 
+app.MapGet("/results", [Authorize] ([FromServices] IParsedScheduleDataService parsedScheduleDataService) => { 
     return parsedScheduleDataService.GetParsedScheduleNoticOfLeases();
 });
 
